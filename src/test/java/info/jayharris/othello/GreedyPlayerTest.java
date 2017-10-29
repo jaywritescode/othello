efc2cd66eacd00d9a3e19693f51819babcf5e5dc
Jay Harris <jaywritescode@users.noreply.github.com>
2017-10-29 10:07:44 -0400

    add greedy heuristic

--- src/test/java/info/jayharris/othello/GreedyPlayerTest.java
+++ src/test/java/info/jayharris/othello/GreedyPlayerTest.java
@@ -1,4 +1,51 @@
 package info.jayharris.othello;
 
+import info.jayharris.othello.Othello.Color;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GreedyPlayerTest {
+
+    Player player;
+    Othello othello;
+
+    static Field boardField;
+
+    @BeforeAll
+    static void init() throws Exception {
+        boardField = Othello.class.getDeclaredField("board");
+        boardField.setAccessible(true);
+    }
+
+    @BeforeEach
+    void initTest() throws Exception {
+        player = new GreedyPlayer(Color.BLACK);
+        othello = new Othello(player, null);
+    }
+
+    @Test
+    @DisplayName("chooses the move that flips the most pieces")
+    void testGetMove() throws Exception {
+        Board board = BoardFactory.instance().fromString(
+                "        " +
+                "        " +
+                "    w   " +
+                "  bbbb  " +
+                "  wbbb  " +
+                "   wwww " +
+                "     b  " +
+                "        "
+        );
+        boardField.set(othello, board);
+
+        System.err.println(board.pretty());
+
+        assertThat(player.getMove(othello)).isSameAs(othello.getBoard().getSquare('d', 7));
+    }
 }
